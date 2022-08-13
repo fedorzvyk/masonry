6ab41bab65bb81caac3fd7991b82ad384cb5b387
fedorzvyk <fedorzvyk@gmail.com>
2022-08-14 01:06:12 +0300

    Initial commit

---
+++ js/masonary.js
@@ -0,0 +1,11 @@
+// external js: masonry.pkgd.js
+
+// jQuery(document).ready(function ($) {
+  $('.grid').masonry({
+  itemSelector: '.grid-item',
+  // horizontalOrder: false,
+  columnWidth: 138,
+    gutter: 7.99,
+  // originRight: false
+  });
+  // });
