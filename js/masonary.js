58f287a5e7ec3f9892ccd8df5450875ca8182780
fedorzvyk <fedorzvyk@gmail.com>
2022-08-15 02:38:41 +0300

    adds adaptive

--- js/masonary.js
+++ js/masonary.js
@@ -1,11 +1,28 @@
 // external js: masonry.pkgd.js
-
+let gut;
+const desktop = window.matchMedia('(min-width: 1200px)')
+const tablet = window.matchMedia('(min-width: 768px)')
+if (desktop.matches) {
+  get = 7.99;
+  // console.log(get);
+} else 
+  
+if (tablet.matches) {
+  get = 6.07;
+  // console.log(get);
+} else get = 2.53;
+console.log(get);
 // jQuery(document).ready(function ($) {
-  $('.grid').masonry({
+$('.grid').masonry({
+
   itemSelector: '.grid-item',
   // horizontalOrder: false,
   // columnWidth: 138,
-    gutter: 7.99
+     gutter: get,
+  stagger: 30,
+     
+   
+
   // originRight: false
   });
   // });
